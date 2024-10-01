97d027326e732e7bfb6f54a3f3535e383e3d286c
Alexis Szerman <alexszer1986@gmail.com>
2024-10-01 20:02:32 -0300

    agregando lineas y detalles

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,4 +1,4 @@
-import Button  from './Button';
+import Button from './Button';
 import ArrowButton from './ArrowButton';
 import BigButton from './BigButton';
 
@@ -6,13 +6,26 @@ const MainSection = () => {
   return (
     <div className="container-fluid p-0" style={{ height: '100vh' }}>
       <div className="row g-0" style={{ height: '100%' }}>
+        
         {/* Columna izquierda */}
-        <div className="col-md-6 d-flex flex-column justify-content-center align-items-center text-center gradient text-white">
-        <Button text='Welcome to Businezz X' className='btn-variant' />
-          <h2>Join us in growing your business.</h2>
-          <p>Elevate your business with Businezz X, a professional Webflow template.</p>
-{/*           <a href="#contact" className="btn btn-light">Contact us</a> */}
-        <ArrowButton text='Contact us'/>
+        <div className="col-md-6 d-flex flex-column justify-content-center align-items-start text-left gradient text-white p-5">
+          <div className='ms-5 mb-5'>
+            <Button text='Welcome to Businezz X' className='btn-variant' />
+            <h2 className="text-start mt-4 big-title">Join us in growing </h2>
+            <h2 className='big-title'>your <span className='bussines'>bussines</span></h2>
+          </div>
+            
+            <div className='mt-5'>
+
+            <div className='ms-5 separator-horizontal'></div>
+
+          <div className='ms-5 mb-5'>
+            <p className="text-start fs-3">
+            Elevate your business with Businezz X, a professional Webflow template.
+            </p>
+            <ArrowButton text='Contact us' />
+          </div>
+            </div>
         </div>
 
         {/* Columna derecha (Formulario con imagen de fondo) */}
@@ -25,22 +38,43 @@ const MainSection = () => {
             height: '100%',
           }}
         >
-          <form style={{ width: '100%', maxWidth: '400px', backgroundColor: 'rgba(255, 255, 255)', padding: '30px' }}>
-            <Button text='Book your free consultation'/>
-          <h4 className='my-4 fw-bold'>Get a free consultation</h4>
+          <form
+            style={{
+              width: '100%',
+              maxWidth: '400px',
+              backgroundColor: 'rgba(255, 255, 255, 0.9)',
+              padding: '30px',
+            }}
+          >
+            <Button text='Book your free consultation' />
+            <h4 className='my-4 fw-bold'>Get a free consultation</h4>
             <div className="row">
-                <div className='col'>
-              <input type="text" className="form-control form-control-lg" id="name" placeholder="Name" />
+              <div className='col'>
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  id="name"
+                  placeholder="Name"
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="email"
+                  className="form-control form-control-lg"
+                  id="email"
+                  placeholder="contact@email.com"
+                />
+              </div>
             </div>
-            <div className="col">
-              <input type="email" className="form-control form-control-lg" id="email" placeholder="contact@email.com" />
-            </div>
-                </div>
             <div className="mb-3">
-              <textarea className="form-control form-control-lg" id="message" rows="3" placeholder="Please type your message here..."></textarea>
+              <textarea
+                className="form-control form-control-lg"
+                id="message"
+                rows="3"
+                placeholder="Please type your message here..."
+              ></textarea>
             </div>
-{/*             <button type="submit" className="btn btn-outline-primary btn-lg">Get in Touch</button> */}
-            <BigButton text='Get in Touch'/>
+            <BigButton text='Get in Touch' />
           </form>
         </div>
       </div>
@@ -49,5 +83,3 @@ const MainSection = () => {
 };
 
 export default MainSection;
-
-
