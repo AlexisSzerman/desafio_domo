d0dca71d749be58f9f688d9807e6777e9d756b29
Alexis Szerman <alexszer1986@gmail.com>
2024-10-01 23:12:56 -0300

    footer listo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,6 +23,7 @@ const Footer = () => {
         /> 
         <p className="mt-4">
           El desarrollo de esta maqueta fue muy interesante porque en general copiar algo supone todo un desafío. Me gustó el concepto de hacer una copia de alta fidelidad, de ahí que con IA regeneré la imagen de fondo para que quede lo más original posible y en Illustrator vectoricé el logo para después poder hacerle una animación simple. Gracias a que los componentes de Bootstrap son responsive se puede generar una version móvil de forma rápida y efectiva, agregando algunos retoques. Espero les guste mi versión y puedan contactarme. Gracias por la oportunidad!
+        
         </p>
       </div>
     </footer>
