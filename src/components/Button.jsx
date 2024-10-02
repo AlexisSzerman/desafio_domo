39c45f63cc16b5d02dd48572e56afe863f2c6360
Alexis Szerman <alexszer1986@gmail.com>
2024-10-01 21:58:27 -0300

    footer creado, falta responsive

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,33 +1,3 @@
-/* import '../styles/Button.css'; // Importa el archivo CSS para aplicar el estilo
-import PropTypes from 'prop-types';
-
-
-const Button = ({ text, type = "button" }) => {
-
-  return (
-    <button
-      type={type}
-      className="btn custom-btn fw-bold">
-      {text}
-    </button>
-  );
-};
-
-export default Button;
-
-
-// Validación de las props
-Button.propTypes = {
-    text: PropTypes.string.isRequired, 
-    type: PropTypes.string,           
-  };
-  
-  // Valores por defecto para las props opcionales
-Button.defaultProps = {
-    type: 'button',
-  }; */
-
-
 import PropTypes from 'prop-types';
 import '../styles/Button.css'; // Importa el archivo CSS para aplicar el estilo
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,34 @@
+import Button from "./Button";
 
-
-function Footer() {
+const Footer = () => {
   return (
-    <div>Soy AleXis Szerman, denme laburo</div>
-  )
-}
+    <footer className="bg-light text-dark text-center py-4">
+      <div className="container">
+        <h6>Creado por</h6>
+        <h4>Alexis Szerman</h4>
+        <p>
+          Email: alexszer1986@gmail.com <br />
+          Cel: +549-11-4160-1797<br />
+          <a href="https://www.linkedin.com/in/alexis-szerman/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin me-2"></i>
+          </a>
+          <a href="https://github.com/AlexisSzerman/desafio_domo" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i> 
+          </a>
+          <br />
+        </p>
+        <Button 
+          text="Descargar CV"
+          onClick={() => window.open('https://drive.usercontent.google.com/u/0/uc?id=1riNeR6WoKa3-OmfQebOadSslmgVxlP_7&export=download', '_blank')} 
+        /> 
+        
+        <p className="mt-4">
+          El desarrollo de esta maqueta fue muy divertido porque en general copiar algo supone todo un desafío. Me gustó el concepto de hacer una copia de alta fidelidad, de ahí que con IA regeneré la imagen de fondo para que quede lo mejor posible y en Illustrator vectoricé el logo para despues poder hacerle una animación simple.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
+
